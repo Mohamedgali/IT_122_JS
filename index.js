53568fccc226c2de92e3165794fd93b928f56877
Mohamed Ali <mohamedali@Mohameds-MacBook-Pro.local>
2020-07-23 20:19:47 -0700

    added

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict'
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const movies = require("./data");
 
 const app = express();
 const exphbs = require('express-handlebars');
 
-app.engine('handlebars', exphbs({
+app.engine('handlebars', exphbs({ 
     defaultLayout: false
 }));
 
@@ -21,25 +21,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const showMovies = movies.getAll();
 
-// Route path for home page. renders home.handlebars in the response, along with dynamic content from the exported data getter method
 app.get('/', (req, res) => {
     res.type('text/html');
     res.render('home', { movies: showMovies });
 });
 
-// Route path for detail page. Renders detail.handlebars, with the movie title passed in the query. dynamic content is passed including the title in the query, and the getDetail method with the title in the query as a parameter
 app.get('/detail', (req, res) => {
     const movietitle = req.query.title
-    res.render('detail', { title: movietitle, stats: movies.getItem(movietitle) });
+    res.render('detail', { title: movietitle, stats: movies.getDetail(movietitle) });
 });
 
-// Send plain text response
 app.get('/about', (req, res) => {
     res.type('text/plain');
     res.send('About page\n My name is Mohamed and I am pursuing programming degree.');
 });
 
-// Define 404 handler
 app.use((req, res) => {
     res.type('text/plain');
     res.status(404);
