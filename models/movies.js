2344d03b05eb06276bd23cfbb818ffc208fc0d4a
Mohamed Ali <mohamedali@Mohameds-MacBook-Pro.local>
2020-07-25 16:13:39 -0700

    add

--- models/movies.js
+++ models/movies.js
@@ -1,7 +1,7 @@
 const credentials = require("../lib/credentials");
 const mongoose = require("mongoose");
 
-mongoose.connect(credentials.connectionString, { dbName: "sccprojects", useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(credentials.connectionString, { dbName: "sccprojects", useNewUrlParser: true });
 
 mongoose.connection.on('open', () => {
     console.log('Mongoose connected.')
