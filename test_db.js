69943ced7eff8e6e1771c4303518d51bee8ef0b6
Mohamed Ali <mohamedali@Mohameds-MacBook-Pro.local>
2020-07-25 17:13:22 -0700

    add

---
+++ test_db.js
@@ -0,0 +1,18 @@
+const Movie = require("../models/movie");
+
+
+Movie.find({}, (err, result) => {
+    // output error if one occurred
+    if (err) {
+        console.log(err);
+    } else {
+        // otherwise output the array of documents
+        console.log(result);
+    }
+});
+
+
+
+
+
+
