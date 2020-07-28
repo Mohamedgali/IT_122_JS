26241640b343d59fa017501b18e5f44c7a93ce69
Mohamed Ali <mohamedali@Mohameds-MacBook-Pro.local>
2020-07-28 11:35:30 -0700

    add

--- data.js
+++ data.js
@@ -6,9 +6,10 @@ const movies = [
    { title: 'There Will Be Blood', year: 2007, actors: 'Daniel Day-Lewis', profit: 5000 },
 ];
 
-exports.getAll = function getAll() {
+exports.getAll = () => {
    return movies;
 }
+
 
 // getItem - should return full data about the requested item
 exports.getDetail = title => {
