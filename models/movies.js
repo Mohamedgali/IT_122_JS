d5dae93d2d7f6f8d9e588aff13041520d38f1d07
Mohamed Ali <mohamedali@Mohameds-MacBook-Pro.local>
2020-08-06 05:06:04 -0700

    update and add REST API's

--- models/movies.js
+++ models/movies.js
@@ -1,8 +1,17 @@
+// reference db credentials
 const credentials = require("../lib/credentials");
+
+// require mongoose ODM library
 const mongoose = require("mongoose");
 
+//connect to db via the credentials info
 mongoose.connect(credentials.connectionString, { dbName: "sccprojects", useNewUrlParser: true, useUnifiedTopology: true });
 
+// Required mongodb/mongoose setting for using findOneAndDelete and findOneAndUpdate
+mongoose.set('useFindAndModify', false);
+
+
+// when mongoose connects to mongodb, display confirmation in console
 mongoose.connection.on('open', () => {
     console.log('Mongoose connected.')
 });
